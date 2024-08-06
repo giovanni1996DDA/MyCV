3c1d1d81cedfd16b1e6029e65ce45fa167951f0e
Juan Oficina <juacdiazda@gmail.com>
2024-08-06 11:47:44 -0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HeaderPage from './Pages/HeaderPage';
 import MainPage from './Pages/MainPage';
 import LoadingPage from './Pages/LoadingPage';
 import { ScrollProvider } from './Hooks/ScrollContext';
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -34,12 +35,12 @@ function App() {
     </ScrollProvider>
   );
   /*return (
-    <div className='flex flex-col justify-center items-center w-1/2 m-auto min-h-screen gap-5'>
-      <h1 className='text-9xl font-bold tracking-tight sm:text-5xl text-cyan-400'>
+    <div className='flex flex-col justify-center items-center w-4/5 m-auto min-h-screen gap-16'>
+      <h1 className='font-bold tracking-tight sm:text-5xl text-cyan-400' style={{fontSize:'7rem'}}>
         Juan Cruz Díaz de Argandoña
       </h1>
-      <div className='flex items-center rounded-full bg-cyan-300/15 px-3 py-1 text-xs font-medium leading-5 text-cyan-300 '>
-        <span className='text-4xl mt-1 mb-2 mr-3 ml-3'>Software Developer</span>
+      <div className='flex items-center rounded-full bg-cyan-300/15  text-cyan-300 '>
+        <p className='mt-1 mb-4 mr-14 ml-14' style={{fontSize:'5rem'}}>Software Developer</p>
       </div>
     </div>
   )*/
